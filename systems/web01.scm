8b18d25852c628e3683733956e94bc29bca9d329
prop4n <legrandenzo.pro@gmail.com>
2026-08-31 19:41:00 +0200

    Remove debugging access and the local test machine

--- systems/web01.scm
+++ systems/web01.scm
@@ -4,13 +4,11 @@
 
 (use-modules (systems base)
              (gnu)
-             (guix gexp)
-             (gnu packages admin)
              (gnu services networking)
              (gnu services web))
 
 (homelab-operating-system
- #:host-name "web-test3"
+ #:host-name "web01"
  #:networking
  (list (service static-networking-service-type
                 (list (static-networking
@@ -24,27 +22,7 @@
                                (gateway "192.168.1.1"))))
                        (name-servers '("1.1.1.1"))))))
  #:extra-services
- (list ;; Debugging: set a known root password so the console login works.
-       (simple-service 'debug-root-password activation-service-type
-         #~(begin
-             (use-modules (ice-9 popen))
-             (let ((port (open-pipe* OPEN_WRITE
-                                     #$(file-append shadow "/sbin/chpasswd"))))
-               (display "root:debug\n" port)
-               (close-pipe port))))
-       ;; Debugging: authorise an ssh key for root (key-only login).
-       (simple-service 'debug-root-ssh-key activation-service-type
-         (with-imported-modules '((guix build utils))
-           #~(begin
-               (use-modules (guix build utils))
-               (mkdir-p "/root/.ssh")
-               (chmod "/root/.ssh" #o700)
-               (call-with-output-file "/root/.ssh/authorized_keys"
-                 (lambda (port)
-                   (display "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINNfWie5JNTDK1pj5OL8w/My5O8G4vA9BAw7vjyWwSF+ proxmops-debug\n"
-                            port)))
-               (chmod "/root/.ssh/authorized_keys" #o600))))
-       (service nginx-service-type
+ (list (service nginx-service-type
                 (nginx-configuration
                  (server-blocks
                   (list (nginx-server-configuration
