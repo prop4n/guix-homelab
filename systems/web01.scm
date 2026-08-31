76acc33683a23bc34b04b1ee73fe37651044e38e
prop4n <legrandenzo.pro@gmail.com>
2026-08-31 16:18:42 +0200

    test2

--- systems/web01.scm
+++ systems/web01.scm
@@ -8,7 +8,7 @@
              (gnu services web))
 
 (homelab-operating-system
- #:host-name "web02"
+ #:host-name "web01"
  #:networking
  (list (service static-networking-service-type
                 (list (static-networking
