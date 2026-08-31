aceeca01a7784eca369015e5d578c48be41ec181
prop4n <legrandenzo.pro@gmail.com>
2026-08-31 20:47:26 +0200

    debug: static IP

--- systems/debug.scm
+++ systems/debug.scm
@@ -1,13 +1,23 @@
 ;;; SPDX-License-Identifier: GPL-3.0-or-later
-;; Minimal debug machine: root ssh key + password, no nginx, DHCP.
-;; Used to get a shell and investigate why a real reconfigure is slow.
+;; Minimal debug machine: static IP .210, root ssh key + password, no nginx.
 (use-modules (systems base)
              (gnu)
              (guix gexp)
-             (gnu packages admin))
+             (gnu packages admin)
+             (gnu services networking))
 
 (homelab-operating-system
  #:host-name "debug"
+ #:networking
+ (list (service static-networking-service-type
+                (list (static-networking
+                       (addresses (list (network-address
+                                         (device "eth0")
+                                         (value "192.168.1.210/24"))))
+                       (routes (list (network-route
+                                      (destination "default")
+                                      (gateway "192.168.1.1"))))
+                       (name-servers '("1.1.1.1"))))))
  #:extra-services
  (list (simple-service 'debug-root-password activation-service-type
          #~(begin
